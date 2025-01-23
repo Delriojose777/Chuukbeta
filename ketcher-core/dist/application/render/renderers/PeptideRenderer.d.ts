0a86c1351bc61057882b116af7698c5e6716abdb
Delriojose777 <josealedelriom@gmail.com>
2025-01-23 02:03:27 -0700

    Subida inicial del proyecto Chúuk Note

---
+++ ketcher-core/dist/application/render/renderers/PeptideRenderer.d.ts
@@ -0,0 +1,19 @@
+import { Selection } from 'd3';
+import { Peptide } from "../../../domain/entities/Peptide";
+import { BaseMonomerRenderer } from "./BaseMonomerRenderer";
+export declare class PeptideRenderer extends BaseMonomerRenderer {
+    monomer: Peptide;
+    CHAIN_BEGINNING: string;
+    constructor(monomer: Peptide, scale?: number);
+    protected appendBody(rootElement: Selection<SVGGElement, void, HTMLElement, never>, theme: any): Selection<SVGUseElement, this, HTMLElement, never>;
+    get textColor(): any;
+    show(theme: any): void;
+    get enumerationElementPosition(): {
+        x: number;
+        y: number;
+    };
+    get beginningElementPosition(): {
+        x: number;
+        y: number;
+    };
+}
